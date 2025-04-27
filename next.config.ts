f8cd3fa7026bbc1f944335c6981222e6def26182
Mingyang Li <mingyangli118@gmail.com>
2025-04-28 11:42:09 +1200

    test redirect from qr code

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  redirects: async () => [
+    {
+      source: "/:path*",
+      has: [
+        {
+          type: "host",
+          value: "https://qr-nextjs-redirect-virid.vercel.app",
+        },
+      ],
+      destination: "https://nextjs-redirect-virid.vercel.app/:path*?source=qr-code",
+      permanent: true,
+    },
+  ],
 };
 
 export default nextConfig;
